63f411b934d74fbebed429309dc222310b1ad325
Siyaman <siyaman.sadagopan@bytedance.com>
2026-01-29 18:15:46 +0800

    Add missing import

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Model3D from "./Model3D";
 import { Product } from "../data/products";
 import "./ProductCard.css";
