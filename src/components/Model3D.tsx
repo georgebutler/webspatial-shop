5a068e7b0ecc437f1453d9ac1df6090da91dd833
Siyaman <siyaman.sadagopan@bytedance.com>
2026-02-03 12:52:25 +0800

    Revert "Make whole product card a link to detail page"
    This reverts commit 9d99bbb8a7039104bddc711ee11a6cfed564966c.

--- src/components/Model3D.tsx
+++ src/components/Model3D.tsx
@@ -4,12 +4,11 @@ import "./Model3D.css";
 export type Model3DProps = ModelProps & {
   src: string;
   imgSrc: string;
-  alt: string;
 }
-export default function Model3D({ className, imgSrc, alt, ...restProps }: Model3DProps) {
+export default function Model3D({ className, imgSrc, ...restProps }: Model3DProps) {
   return (
     <Model enable-xr className={`model3D ${className}`} {...restProps}>
-      <img src={imgSrc} alt={alt} />
+      <img src={imgSrc} />
     </Model>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,21 +3,29 @@ import Model3D from "./Model3D";
 import { Product } from "../data/products";
 import "./ProductCard.css";
 
-export type ProductCardProps = { product: Product; }
+interface ProductCardProps {
+  product: Product;
+}
+
 export function ProductCard({ product }: ProductCardProps) {
   const navigate = useNavigate();
+
   return (
-    <Link to={`/product/${product.id}`} className="product-card">
+    <div className="product-card">
       <Model3D 
         className="product-3D"
         src={product.model} 
         imgSrc={product.image}
-        alt={product.name}
         onSpatialTap={() => navigate(`/product/${product.id}`)} 
       />
       <h3 className="product-name">{product.name}</h3>
       <p className="product-description">{product.description}</p>
       <p className="product-price">${product.price.toFixed(2)}</p>
-    </Link>
+      <div className="product-card-footer">
+        <Link to={`/product/${product.id}`}>
+          Go to product
+        </Link>
+      </div>
+    </div>
   );
 }
