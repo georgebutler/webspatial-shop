f4393981e5c0480d60f95706d8f0fa4527145b39
Siyaman <siyaman.sadagopan@bytedance.com>
2026-02-16 13:23:13 -0800

    Code cleanup

--- src/components/Model3D.tsx
+++ src/components/Model3D.tsx
@@ -1,14 +1,16 @@
-import { Model, ModelProps } from "@webspatial/react-sdk";
+import type { Ref } from "react";
+import { Model, ModelRef, ModelProps } from "@webspatial/react-sdk";
 import "./Model3D.css";
 
 export type Model3DProps = ModelProps & {
   src: string;
   imgSrc: string;
   alt: string;
+  ref?: Ref<ModelRef>;
 }
-export default function Model3D({ className, imgSrc, alt, ...restProps }: Model3DProps) {
+export default function Model3D({ className, imgSrc, alt, ref, ...restProps }: Model3DProps) {
   return (
-    <Model enable-xr className={`model3D ${className}`} {...restProps}>
+    <Model enable-xr className={`model3D ${className}`} ref={ref} {...restProps}>
       <img src={imgSrc} alt={alt} />
     </Model>
   );
