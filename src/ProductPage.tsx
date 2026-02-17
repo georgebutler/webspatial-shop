0d67de2a58f710f62843b221033c2430de080d63
Siyaman <siyaman.sadagopan@bytedance.com>
2026-02-17 14:06:05 -0800

    Replace react-router-dom with react-router

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router";
 import { products, Product } from "./data/products";
 import "./ProductPage.css";
 import {
