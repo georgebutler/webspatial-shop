3e87a09d41da107f3885b95c28b3d34badaa313d
Siyaman <siyaman.sadagopan@bytedance.com>
2026-02-18 15:33:54 -0800

    Remove vite-plugin-html
    vite-plugin-html has a vulnerability and is not required for this setup
    since the web and avp packages are combined.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import webspatial from "@webspatial/vite-plugin";
-import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   plugins: [
     react(),
     webspatial({ outputDir: "/" }),
-    createHtmlPlugin({ inject: { data: { XR_ENV: process.env.XR_ENV } } }),
   ],
   server: { open: true, host: true },
 });
