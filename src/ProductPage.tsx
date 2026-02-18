f1673730bceba27057b038fa1fe561713e20e172
Siyaman <siyaman.sadagopan@bytedance.com>
2026-02-18 11:25:35 -0800

    Replace Reality component with Model3D

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -1,28 +1,23 @@
 import { Link, useParams } from "react-router";
-import { products, Product } from "./data/products";
+import { products } from "./data/products";
 import "./ProductPage.css";
-import {
-  ModelAsset,
-  ModelEntity,
-  Reality,
-  SceneGraph,
-  UnlitMaterial,
-} from "@webspatial/react-sdk";
-import { useEffect, useState } from "react";
+import { ModelRef } from "@webspatial/react-sdk";
+import { useEffect, useRef } from "react";
+import Model3D from "./components/Model3D";
 
 export default function ProductPage() {
   const { id } = useParams<{ id: string }>();
-  const product: Product | undefined = products.find((p) => p.id === id);
-  const [rotation, setRotation] = useState({ x: 0, y: 0, z: 0 });
+  const product = products.find((p) => p.id === id);
+  const modelRef = useRef<ModelRef>(null);
   useEffect(() => {
     let id: number;
     function animate() {
-      setRotation((prev) => ({ ...prev, y: prev.y + 1.0 }));
+      modelRef.current?.entityTransform?.rotateSelf(0, 1, 0);
       id = requestAnimationFrame(animate);
     }
     animate();
     return () => cancelAnimationFrame(id);
-  }, []);
+  }, [modelRef.current]);
 
   if (!product) {
     return (
@@ -39,34 +34,18 @@ export default function ProductPage() {
         <h1 className="product-name">
           {product.name}
         </h1>
-        <Reality style={{ width: "500px", height: "450px", "--xr-depth": 100 }}>
-          <UnlitMaterial id="red" color="#ff0000" />
-          <ModelAsset
-            id={product.model}
-            src={product.model}
-          />
-          <SceneGraph>
-            <ModelEntity
-              model={product.model}
-              position={{ ...product.position, z: product.position.z + 0.1 }}
-              scale={{
-                x: product.scale.x * 2,
-                y: product.scale.y * 2,
-                z: product.scale.z * 2,
-              }}
-              rotation={{
-                x: product.rotation.x,
-                y: product.rotation.y + rotation.y,
-                z: product.rotation.z,
-              }}
-            />
-          </SceneGraph>
-        </Reality>
-        <article className="description">
+        <Model3D
+          className="product-3D"
+          src={product.model}
+          imgSrc={product.image}
+          alt={product.name}
+          ref={modelRef}
+        />
+        <article>
           <h2>Description</h2>
-          <p className="product-description">{product.description}</p>
-          <p className="product-price">${product.price.toFixed(2)}</p>
-          <p><Link to="/">Back to Store</Link></p>
+          <p>{product.description}</p>
+          <p>${product.price.toFixed(2)}</p>
+          <Link to="/">Back to Store</Link>
         </article>
       </div>
     </div>
